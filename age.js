12f27c43b19481e5979481cfad66c3c6389566c7
TeeAdeyemi <145777853+TeeAdeyemi@users.noreply.github.com>
2023-10-12 10:18:53 +0100

    Add files via upload

---
+++ age.js
@@ -0,0 +1,18 @@
+ let myName = ("Adeyemi")
+
+ let hun = 100
+ 
+ const myAge = prompt ("How old are you")
+ 
+ let myDays = myAge*365.25;
+
+ let myWeeks = myAge*52;
+
+ let myHours = myDays*24
+
+ let mySeconds = myDays*86400
+
+ const myHundred = 1 + myAge
+
+
+alert (myName +  ", " +"you have lived"+   " "  + myDays+ "days,"+   " " + myWeeks + "weeks," +   " " + myHours +"hours," +  " " + mySeconds + "seconds." +   " " +"And you will be" + " " + myHundred + " " +"in hundred years." )
